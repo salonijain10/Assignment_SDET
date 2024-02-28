0df4e1cd05d343862633d6c2717389c107c5ee9c
salonijain10 <67259410+salonijain10@users.noreply.github.com>
2024-02-28 13:14:07 +0530

    Add files via upload

---
+++ TestFirst.java
@@ -0,0 +1,108 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class TestFirst {
+	public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        // Create a new instance of the Chrome driver
+        WebDriver driver = new ChromeDriver();
+
+        // Launch the URL
+        driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html");
+        
+        // Wait for 2 seconds
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        
+        // Find the Table Data button and click it
+        WebElement tableDataButton = driver.findElement(By.xpath ("//*[contains(text(),'Table Data')]"));
+        tableDataButton.click();
+        
+        // Insert data into the input text box using JavaScript
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String jsonData = "[{\"name\" : \"Bob\", \"age\" : 20, \"gender\": \"male\"}, {\"name\": \"George\", \"age\" : 42, \"gender\": \"male\"}, {\"name\": \"Sara\", \"age\" : 42, \"gender\": \"female\"}, {\"name\": \"Conor\", \"age\" : 40, \"gender\": \"male\"}, {\"name\": \"Jennifer\", \"age\" : 42, \"gender\": \"female\"}]";
+        js.executeScript("document.getElementById('jsondata').value='" + jsonData + "'");
+        
+     // Wait for 2 seconds
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Click on the Refresh Table button
+        WebElement refreshButton = driver.findElement(By.xpath ("//*[contains(text(),'Refresh Table')]"));
+        refreshButton.click();
+                
+        // Wait for 2 seconds for the table to be visible
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        
+        // Get the table rows
+        List<WebElement> rows = driver.findElements(By.xpath("//table[@id='dynamictable']//tbody//tr"));
+        
+     
+        // Assert the data in the table with the inserted data
+        String[][] expectedData = {
+                {"Bob", "20", "male"},
+                {"George", "42", "male"},
+                {"Sara", "42", "female"},
+                {"Conor", "40", "male"},
+                {"Jennifer", "42", "female"}
+        };
+
+        boolean dataMatches = true;
+        int rowNumber = 1;
+        for (WebElement row : rows) {
+            List<WebElement> columns = row.findElements(By.tagName("td"));
+            for (int i = 0; i < columns.size(); i++) {
+                if (!columns.get(i).getText().equals(expectedData[rowNumber - 1][i])) {
+                    dataMatches = false;
+                    break;
+                }
+            }
+            if (!dataMatches) {
+                break;
+            }
+            rowNumber++;
+        }
+
+        // Assert the data
+        if (dataMatches) {
+            System.out.println("Data matches!");
+        } else {
+            System.out.println("Data does not match!");
+        }       
+
+        // Wait for 2 seconds
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        
+        // Close the browser
+        driver.close();
+    }
+
+
+}
